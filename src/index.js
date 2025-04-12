be55e24e8d9161ad715b7d493ed086444820f796
younnlei <younl.eain@gmail.com>
2025-04-11 23:39:59 -0400

    Crate basic http server

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const server = http.createServer( (req, res) => {
+    if (req.method === 'GET' && req.url === '/') { 
+        console.log('hello from server')  
+        res.end()
+    }
+
+})
+
+server.listen(3001, () => {
+    console.log('server on http://localhost:3001')
+})
